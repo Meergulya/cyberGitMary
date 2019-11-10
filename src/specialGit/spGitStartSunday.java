a6bf0841cc0bde16c18f75d9f22ddd907a42fd2d
Meergul <mirgulyar@gmail.com>
2019-11-10 11:37:26 -0600

    just change the first line

---
+++ src/specialGit/spGitStartSunday.java
@@ -0,0 +1,16 @@
+package specialGit;
+
+public class spGitStartSunday {
+
+
+    public static void main(String[] args) {
+        System.out.println("Good Morning America");
+
+
+   
+    }
+
+
+
+
+}
