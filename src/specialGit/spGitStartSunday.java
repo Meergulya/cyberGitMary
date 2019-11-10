00d8751d2b46a3fb749f1eebd6b48514f6b565a2
Meergul <mirgulyar@gmail.com>
2019-11-10 11:39:50 -0600

    just change the first line and add a new line

--- src/specialGit/spGitStartSunday.java
+++ src/specialGit/spGitStartSunday.java
@@ -6,8 +6,9 @@ public class spGitStartSunday {
     public static void main(String[] args) {
         System.out.println("Good Morning America");
 
+        System.out.println("Good Morning Germany");
+        System.out.println("Good Morning New Australia");
 
-   
     }
 
 
